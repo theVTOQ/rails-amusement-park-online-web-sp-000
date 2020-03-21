8b60d2f396b1cd90a585d821a9e4271da9b29a93
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-03-21 23:48:54 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      binding.pry
+      #binding.pry
       render "new"
     end
   end
